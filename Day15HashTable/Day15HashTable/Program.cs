4c892db729606d1c1599cdbf054d9ad130c4e14c
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-08-05 17:15:33 +0530

    [pavan] frequency

---
+++ Day15HashTable/Day15HashTable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using Day15HashTable;
+
+class Program
+{
+    MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+    public void Frequency(string str)
+    {
+
+        // Split the string by spaces
+        string[] a = str.Split(' ');
+        for (int j = 0; j < a.Length; j++)
+        {
+            string word = a[j];
+            int count = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (word.Equals(a[i]))
+                    count++;
+            }
+            Console.WriteLine($"The frequency of {a[j]} is {count}");
+            hash.Add($"{j}", word);
+            Console.WriteLine($"Key - {j} Value - {hash.Get($"{j}")}");
+        }
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to HashTable and Binary search Tree programs");
+        string str = "To be or not to be";
+        Program p = new Program();
+        p.Frequency(str);
+        Console.WriteLine("Enter any index from 0 to 5 to find the value");
+        int j = Convert.ToInt32(Console.ReadLine());
+        string n = p.hash.Get($"{j}");
+        Console.WriteLine($"\nIndex {j}'s value: {n}");
+    }
+}
